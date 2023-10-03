6b75f55d08ab4a624017070fcde742bb5a36d64f
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-03 11:22:27 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        MsgBox("2+3=" & 2 + 3)
+        MsgBox("5-3=" & 5 - 3)
+        MsgBox("2/3=" & 2 / 3)
+        MsgBox("2*3=" & 2 * 3)
+    End Sub
+End Class
